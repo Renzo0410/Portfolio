32a0776ebca265927c29eb4fb2c964941257a1a5
Renzo Gabriel <renzo.gabrielch@gmail.com>
2025-06-18 00:57:37 +0200

    Last commit

--- portfolio-01/js/modals.js
+++ portfolio-01/js/modals.js
@@ -1,12 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const modalCount = 3; // Cambiar este número si se agregan más.
+    const modalCount = 4; // Cambiar este número si se agregan más.
     const container = document.getElementById('modalContainer');
 
+    // Detectar si estamos en /views/projects.html o no
+    const isInViews = window.location.pathname.includes('/views/');
+
+    // Ajustar ruta base para fetch()
+    const basePath = isInViews ? '../modal/' : './modal/';
+
     for (let i = 1; i <= modalCount; i++) {
-        fetch(`./modal/modal-p0${i}.html`)
+        fetch(`${basePath}modal-p0${i}.html`)
             .then(res => res.text())
             .then(html => {
                 container.innerHTML += html;
-            });
+            })
+            .catch(err => console.error(`Error cargando modal ${i}:`, err));
     }
 });
